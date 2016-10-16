9226168fdd194b3b973aacbd620c7b8f838daf57
disturm <disturm@hotmail.com>
2016-10-16 22:27:22 +0500

    fix solved

--- Emails/EmailMessage.cs
+++ Emails/EmailMessage.cs
@@ -4,8 +4,8 @@ namespace Emails
 {
 	public class EmailMessage
 	{
-		public string Subject;
-		public StringBuilder Body = new StringBuilder();
-		public bool Important;
+	    public bool Important;
+	    public string Subject;
+	    public StringBuilder Body = new StringBuilder();
 	}
 }
